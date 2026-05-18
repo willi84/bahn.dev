194faf1d4df7f6d2b3639ae56f0d8c3021e41281
Robert Willemelis <willi84@users.noreply.github.com>
2026-05-18 14:53:50 +0200

    Write station detail API output as .json files and update links

--- src/backend/apps/stations/db-open-stations/db-open-stations.ts
+++ src/backend/apps/stations/db-open-stations/db-open-stations.ts
@@ -101,7 +101,7 @@ export const getOpenStationAPI = () => {
     for (const dhid in stops) {
         const stop = stops[dhid];
         const encodedDhid = encodeURIComponent(dhid);
-        const stopFilePath = `${siteStationsPath}/${encodedDhid}/index.html`;
+        const stopFilePath = `${siteStationsPath}/${encodedDhid}.json`;
         const stopContent = JSON.stringify({ dhid, data: stop }, null, 2);
         FS.writeFile(stopFilePath, stopContent);
         stopFileSizesKB[dhid] = toKB(FS.sizeContent(stopContent));
